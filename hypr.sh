d0fd07326098ed498a19059756035f39287c7356
sciencywolf <sciencywolf@users.noreply.github.com>
2016-10-28 00:42:21 +0200

    Update hypr.sh

--- hypr.sh
+++ hypr.sh
@@ -6,4 +6,4 @@ yres=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)
 nxres=$(echo $(echo $xres \* $r +0.5| bc) / 1 | bc)
 nyres=$(echo $(echo $yres \* $r +0.5| bc) / 1 | bc)
 output=$(xrandr --current | grep ' connected' | uniq | awk '{print $1}' | cut -d ' ' -f1)
-xrandr --output $output --mode $xres"x"$yres --scale $r"x"$r --panning $nxres"x"$nyres
+xrandr -d :0 --output $output --mode $xres"x"$yres --crtc 1 --scale-from $nxres"x"$nyres --panning $nxres"x"$nyres
